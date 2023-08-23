f68567a4756d81b2fc206f75e3fa75e4e3a7fa6c
hukm <maximgook@gmail.com>
2023-08-23 13:49:07 +0200

    Maven project init

---
+++ src/main/java/com/avispl/symphony/dal/communicator/aggregator/App.java
@@ -0,0 +1,13 @@
+package com.avispl.symphony.dal.communicator.aggregator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
