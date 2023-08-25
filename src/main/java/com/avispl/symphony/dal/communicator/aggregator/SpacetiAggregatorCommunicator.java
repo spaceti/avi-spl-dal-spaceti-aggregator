69a5ee7a648e2118c918fef844c266dc1c193f91
hukm <maximgook@gmail.com>
2023-08-25 17:01:29 +0200

    Working PoC

--- src/main/java/com/avispl/symphony/dal/communicator/aggregator/SpacetiAggregatorCommunicator.java
+++ src/main/java/com/avispl/symphony/dal/communicator/aggregator/SpacetiAggregatorCommunicator.java
@@ -1,72 +1,192 @@
 package com.avispl.symphony.dal.communicator.aggregator;
 
-import com.avispl.symphony.api.dal.Device;
-import com.avispl.symphony.api.dal.Version;
-import com.avispl.symphony.api.dal.control.Controller;
-import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
 import com.avispl.symphony.api.dal.dto.monitor.aggregator.AggregatedDevice;
 import com.avispl.symphony.api.dal.monitor.aggregator.Aggregator;
-import com.avispl.symphony.api.dal.ping.Pingable;
 import com.avispl.symphony.dal.communicator.RestCommunicator;
+import com.avispl.symphony.dal.aggregator.parser.AggregatedDeviceProcessor;
+import com.avispl.symphony.dal.aggregator.parser.PropertiesMapping;
+import com.avispl.symphony.dal.aggregator.parser.PropertiesMappingParser;
+import com.fasterxml.jackson.databind.JsonNode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import org.apache.commons.lang3.tuple.Pair;
 import java.util.stream.Collectors;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 
 
-public class SpacetiAggregatorCommunicator extends RestCommunicator implements Pingable, Aggregator
+public class SpacetiAggregatorCommunicator extends RestCommunicator implements Aggregator
 {
-    private List<AggregatedDevice> devices = new ArrayList<>();
-
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    /**
+     * Build an instance of SpacetiAggregatorCommunicator
+     * Setup aggregated devices processor, initialize adapter properties
+     *
+     * @throws IOException if unable to locate mapping yml file or properties file
+     */
+    public SpacetiAggregatorCommunicator() throws IOException {
+        Map<String, PropertiesMapping> mapping = new PropertiesMappingParser().loadYML("mapping/model-mapping.yml", getClass());
+        aggregatedDeviceProcessor = new AggregatedDeviceProcessor(mapping);
+        // adapterProperties = new Properties();
+        // adapterProperties.load(getClass().getResourceAsStream("/version.properties"));
     }
 
-    // public void destroy() {
-    //     // this method is called upon a Symphony shutdown
-    //     // here all persistent resources, sockets have to be released
-    //     System.out.printf("Destroying aggregator instance " + SpacetiAggregatorCommunicator.class);
-    // }
+    class SpacetiDeviceDataLoader implements Runnable {
+        private volatile boolean inProgress;
 
-    public boolean isInitialized() {
-        // has to return true once aggregator is fully initialized
-        return true;
+        public SpacetiDeviceDataLoader() {
+            inProgress = true;
+        }
+
+        @Override
+        public void run() {
+            mainloop:
+            while (inProgress) {
+                logger.debug("Main loop");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    // Ignore for now
+                }
+
+                if (!inProgress) {
+                    break mainloop;
+                }
+
+                try {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Fetching devices list");
+                    }
+                    fetchDevicesList();
+                    //knownErrors.remove(ROOMS_LIST_RETRIEVAL_ERROR_KEY);
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Fetched devices list: " + aggregatedDevices);
+                    }
+                } catch (Exception e) {
+                    //knownErrors.put(ROOMS_LIST_RETRIEVAL_ERROR_KEY, limitErrorMessageByLength(e.getMessage(), maxErrorLength));
+                    logger.error("Error occurred during device list retrieval: " + e.getMessage() + " with cause: " + e.getCause().getMessage(), e);
+                }
+
+                if (!inProgress) {
+                    break mainloop;
+                }
+
+                int aggregatedDevicesCount = aggregatedDevices.size();
+                if (aggregatedDevicesCount == 0) {
+                    continue mainloop;
+                }
+
+                while (nextDevicesCollectionIterationTimestamp > System.currentTimeMillis()) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(1000);
+                    } catch (InterruptedException e) {
+                        //
+                    }
+                }
+
+                // try {
+                //     // The following request collect all the information, so in order to save number of requests, which is
+                //     // daily limited for certain APIs, we need to request them once per monitoring cycle.
+                //     retrieveZoomRoomMetrics();
+                //     knownErrors.remove(ROOMS_METRICS_RETRIEVAL_ERROR_KEY);
+                // } catch (Exception e) {
+                //     knownErrors.put(ROOMS_METRICS_RETRIEVAL_ERROR_KEY, limitErrorMessageByLength(e.getMessage(), maxErrorLength));
+                //     logger.error("Error occurred during ZoomRooms metrics retrieval: " + e.getMessage() + " with cause: " + e.getCause().getMessage());
+                // }
+
+                // for (AggregatedDevice aggregatedDevice : aggregatedDevices.values()) {
+                //     if (!inProgress) {
+                //         break;
+                //     }
+                //     devicesExecutionPool.add(executorService.submit(() -> {
+                //         String deviceId = aggregatedDevice.getDeviceId();
+                //         try {
+                //             populateDeviceDetails(deviceId);
+                //             knownErrors.remove(deviceId);
+                //         } catch (Exception e) {
+                //             knownErrors.put(deviceId, limitErrorMessageByLength(e.getMessage(), maxErrorLength));
+                //             logger.error(String.format("Exception during Zoom Room '%s' data processing.", aggregatedDevice.getDeviceName()), e);
+                //         }
+                //     }));
+                // }
+
+                // do {
+                //     try {
+                //         TimeUnit.MILLISECONDS.sleep(500);
+                //     } catch (InterruptedException e) {
+                //         if (!inProgress) {
+                //             break;
+                //         }
+                //     }
+                //     devicesExecutionPool.removeIf(Future::isDone);
+                // } while (!devicesExecutionPool.isEmpty());
+
+                // We don't want to fetch devices statuses too often, so by default it's currentTime + 30s
+                // otherwise - the variable is reset by the retrieveMultipleStatistics() call, which
+                // launches devices detailed statistics collection
+                nextDevicesCollectionIterationTimestamp = System.currentTimeMillis() + 30000;
+
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Finished collecting devices statistics cycle at " + new Date());
+                }
+            }
+            // Finished collecting
+        }
+
+        /**
+         * Triggers main loop to stop
+         */
+        public void stop() {
+            inProgress = false;
+        }
     }
 
-    // public void init() throws Exception {
-    //     // this method is called after instance is created and
-    //     // JavaBean properties are set
-    //     System.out.printf("Initializing aggregator instance " + SpacetiAggregatorCommunicator.class);
 
-    //     devices.addAll(Arrays.asList(
-    //             ConfigAggregatedDevice.createDevice("Lights"),
-    //             ConfigAggregatedDevice.createDevice("Projector"),
-    //             ConfigAggregatedDevice.createDevice("Touchscreen")));
-    // }
+    private static final String SPACETI_DEVICES_URL = "v2/devices";
+    /**
+     * API Token used for authorization in Spaceti API
+     */
+    private volatile String authorizationToken;
+    private ConcurrentHashMap<String, AggregatedDevice> aggregatedDevices = new ConcurrentHashMap<>();
+    /**
+     * Device adapter instantiation timestamp.
+     */
+    private long adapterInitializationTimestamp;
+    /**
+     * Executor that runs all the async operations, that {@link #deviceDataLoader} is posting and
+     * {@link #devicesExecutionPool} is keeping track of
+     */
+    private static ExecutorService executorService;
+    /**
+     * Runner service responsible for collecting data and posting processes to {@link #devicesExecutionPool}
+     */
+    private SpacetiDeviceDataLoader deviceDataLoader;
+    /**
+     * Time period within which the device metadata (basic devices information) cannot be refreshed.
+     * Ignored if device list is not yet retrieved or the cached device list is empty {@link SpacetiAggregatorCommunicator#aggregatedDevices}
+     */
+    private volatile long validDeviceMetaDataRetrievalPeriodTimestamp;
+    /**
+     * Whether service is running.
+     */
+    private volatile boolean serviceRunning;
+    /**
+     * Time period within which the device metrics (dynamic information) cannot be refreshed.
+     * Ignored if metrics data is not yet retrieved
+     */
+    private volatile long validMetricsDataRetrievalPeriodTimestamp;
+    private AggregatedDeviceProcessor aggregatedDeviceProcessor;
+    /**
+     * We don't want the statistics to be collected constantly, because if there's not a big list of devices -
+     * new devices statistics loop will be launched before the next monitoring iteration. To avoid that -
+     * this variable stores a timestamp which validates it, so when the devices statistics is done collecting, variable
+     * is set to currentTime + 30s, at the same time, calling {@link #retrieveMultipleStatistics()} and updating the
+     * {@link #aggregatedDevices} resets it to the currentTime timestamp, which will re-activate data collection.
+     */
+    private static long nextDevicesCollectionIterationTimestamp;
 
-    public Version retrieveSoftwareVersion() throws Exception {
-        // version of software / firmware target aggregator is running
-        return new Version("1.12.3");
-    }
 
-    public String getAddress() {
-        // IP address / hostname of the aggregator
-        return "10.0.0.3";
-    }
-
-    public int getPingTimeout() {
-        // ping timeout value
-        return 60;
-    }
-
-    public int ping() throws Exception {
-        // has to perform ping and return ping latency to a target device
-        return new Random().nextInt(40);
-    }
 
     @Override
     public List<AggregatedDevice> retrieveMultipleStatistics() throws Exception {
@@ -76,15 +196,31 @@ public class SpacetiAggregatorCommunicator extends RestCommunicator implements P
         // Code in this sample constructs dummy instances of AggregatedDevice with fake data
         // In real adapter following information needs to be retrieved from the target device and mapped to AggregatedDevice instance
         // using remote network calls
-
-        return Collections.unmodifiableList(devices);
+        if (logger.isDebugEnabled()) {
+            logger.debug(String.format("Adapter initialized: %s, executorService exists: %s, serviceRunning: %s", isInitialized(), executorService != null, serviceRunning));
+        }
+        if (executorService == null) {
+            // Due to the bug that after changing properties on fly - the adapter is destroyed but adapter is not initialized properly,
+            // so executor service is not running. We need to make sure executorService exists
+            executorService = Executors.newFixedThreadPool(8);
+            executorService.submit(deviceDataLoader = new SpacetiDeviceDataLoader());
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug(String.format("Aggregator Multiple statistics requested. Aggregated Devices collected so far: %s. Runner thread running: %s. Executor terminated: %s",
+                    aggregatedDevices.size(), serviceRunning, executorService.isTerminated()));
+        }
+        long currentTimestamp = System.currentTimeMillis();
+        nextDevicesCollectionIterationTimestamp = currentTimestamp;
+        
+        aggregatedDevices.values().forEach(aggregatedDevice -> aggregatedDevice.setTimestamp(currentTimestamp));
+        return new ArrayList<>(aggregatedDevices.values());
     }
 
     @Override
     public List<AggregatedDevice> retrieveMultipleStatistics(List<String> list) throws Exception {
         // same as retrieveMultipleStatistics(), but just for given device identifiers
         return retrieveMultipleStatistics().stream()
-                .filter(list::contains)
+                .filter(aggregatedDevice -> list.contains(aggregatedDevice.getDeviceId()))
                 .collect(Collectors.toList());
     }
 
@@ -98,5 +234,140 @@ public class SpacetiAggregatorCommunicator extends RestCommunicator implements P
     protected void authenticate() throws Exception {
     }
 
+    private void fetchDevicesList() throws Exception {
+        long currentTimestamp = System.currentTimeMillis();
+        if (logger.isDebugEnabled()) {
+            logger.debug("Fetching devices list");
+        }
 
+        List<AggregatedDevice> spacetiDevices = new ArrayList<>();
+        try {
+            processPaginatedSpacetiRetrieval(spacetiDevices);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error occurred during devices list retrieval: " + e.getMessage() + " with cause: " + e.getCause().getMessage(), e);
+        }
+
+        spacetiDevices.forEach(aggregatedDevice -> {
+            String deviceId = aggregatedDevice.getDeviceId();
+            if (aggregatedDevices.containsKey(deviceId)) {
+                //ToDo: update with new data
+                //aggregatedDevices.get(deviceId).setDeviceOnline(aggregatedDevice.getDeviceOnline());
+            } else {
+                aggregatedDevices.put(deviceId, aggregatedDevice);
+            }
+        });
+        // ToDo: remove devices that were not populated by the API
+    }
+
+    /**
+     * Retrieve Spaceti devices with support of the Spaceti API pagination (next page token)
+     *
+     * @param spacetiDevices to save all retrieved rooms to
+     * @throws Exception if any communication error occurs
+     * */
+    private void processPaginatedSpacetiRetrieval(List<AggregatedDevice> spacetiDevices) throws Exception {
+        boolean hasNextPage = true;
+        String nextPageToken = null;
+        Pair<JsonNode, String> response;
+        while(hasNextPage) {
+            // if (logger.isDebugEnabled()) {
+            //     logger.debug(String.format("Receiving page with next_page_token: %s", nextPageToken));
+            // }
+            // response = retrieveZoomRooms(locationId, nextPageToken);
+            // nextPageToken = response.getRight();
+            // hasNextPage = StringUtils.isNotNullOrEmpty(nextPageToken);
+            // zoomRooms.addAll(aggregatedDeviceProcessor.extractDevices(response.getLeft()));
+            response = retrieveSpacetiDevices(nextPageToken);
+            hasNextPage = false;
+            spacetiDevices.addAll(aggregatedDeviceProcessor.extractDevices(response.getLeft()));
+        }
+    }
+
+    /**
+     * Retrieve list of Spaceti devices available
+     *
+     * @param nextPageToken token to reference the next page. Null or empty if shouldn't be applied
+     * @return response pair of JsonNode and next_page_token
+     * @throws Exception if a communication error occurs
+     */
+    private Pair<JsonNode, String> retrieveSpacetiDevices(String nextPageToken) throws Exception {
+        /*StringBuilder queryString = new StringBuilder();
+        if (!StringUtils.isNullOrEmpty(zoomRoomTypes)) {
+            queryString.append("&type=").append(zoomRoomTypes);
+        }
+        if (!StringUtils.isNullOrEmpty(locationId)) {
+            queryString.append("&location_id=").append(locationId);
+        }
+        if (!StringUtils.isNullOrEmpty(nextPageToken)) {
+            queryString.append("&next_page_token=").append(nextPageToken);
+        }*/
+        //JsonNode response = doGetWithRetry(String.format(ZOOM_ROOMS_URL, roomRequestPageSize) + queryString);
+        JsonNode response = doGet(SPACETI_DEVICES_URL, JsonNode.class);
+        if (response == null) {
+            return Pair.of(null, null);
+        }
+        //return Pair.of(response, response.at("/next_page_token").asText());
+        return Pair.of(response, null);
+    }
+     
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected HttpHeaders putExtraRequestHeaders(HttpMethod httpMethod, String uri, HttpHeaders headers) throws Exception {
+        headers.add("Content-Type", "application/json");
+        headers.add("Authorization", "Bearer " + authorizationToken);
+        return super.putExtraRequestHeaders(httpMethod, uri, headers);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void internalInit() throws Exception {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Internal init is called.");
+        }
+        adapterInitializationTimestamp = System.currentTimeMillis();
+        authorizationToken = getPassword();
+
+        executorService = Executors.newFixedThreadPool(8);
+        executorService.submit(deviceDataLoader = new SpacetiDeviceDataLoader());
+
+        long currentTimestamp = System.currentTimeMillis();
+        validDeviceMetaDataRetrievalPeriodTimestamp = currentTimestamp;
+        validMetricsDataRetrievalPeriodTimestamp = currentTimestamp;
+        serviceRunning = true;
+
+        super.internalInit();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void internalDestroy() {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Internal destroy is called.");
+        }
+        serviceRunning = false;
+
+        if (deviceDataLoader != null) {
+            deviceDataLoader.stop();
+            deviceDataLoader = null;
+        }
+
+        if (executorService != null) {
+            executorService.shutdownNow();
+            executorService = null;
+        }
+
+        // devicesExecutionPool.forEach(future -> future.cancel(true));
+        // devicesExecutionPool.clear();
+
+        aggregatedDevices.clear();
+        super.internalDestroy();
+    }  
 }
--- src/test/java/com/avispl/symphony/dal/communicator/aggregator/SpacetiAggregatorCommunicatorTest.java
+++ src/test/java/com/avispl/symphony/dal/communicator/aggregator/SpacetiAggregatorCommunicatorTest.java
@@ -1,20 +1,45 @@
 package com.avispl.symphony.dal.communicator.aggregator;
 
+import com.avispl.symphony.api.dal.dto.control.AdvancedControllableProperty;
+import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
+import com.avispl.symphony.api.dal.dto.monitor.Statistics;
+import com.avispl.symphony.api.dal.dto.monitor.aggregator.AggregatedDevice;
+
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 
 /**
  * Unit test for simple App.
  */
+@Tag("test")
 public class SpacetiAggregatorCommunicatorTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    static SpacetiAggregatorCommunicator mockAggregatorCommunicator;
+
+    @BeforeEach
+    public void init() throws Exception {
+        mockAggregatorCommunicator = new SpacetiAggregatorCommunicator();
+        mockAggregatorCommunicator.setPassword(System.getenv("API2_TOKEN_SPACETIHQ"));
+        mockAggregatorCommunicator.setHost("api.spaceti.net");
+        mockAggregatorCommunicator.setProtocol("https");
+        mockAggregatorCommunicator.setPort(443);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void getDevicesWithFilteringTest() throws Exception {
+        mockAggregatorCommunicator.init();
+        mockAggregatorCommunicator.retrieveMultipleStatistics();
+        Thread.sleep(5000);
+        List<AggregatedDevice> devices = mockAggregatorCommunicator.retrieveMultipleStatistics();
+        Assert.assertFalse(devices.isEmpty());
+        Assert.assertEquals(49, devices.size());
+        Assert.assertNotNull(devices.get(0).getSerialNumber());
+
     }
 }
